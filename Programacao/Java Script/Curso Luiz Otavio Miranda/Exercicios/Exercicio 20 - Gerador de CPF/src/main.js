3c7b32645493d0eb547ddadf05d4e381960b4762
Pedro <pedro.isaak@gmail.com>
2022-05-02 22:23:04 -0300

    init

--- Programacao/Java Script/Curso Luiz Otavio Miranda/Exercicios/Exercicio 20 - Gerador de CPF/src/main.js
+++ Programacao/Java Script/Curso Luiz Otavio Miranda/Exercicios/Exercicio 20 - Gerador de CPF/src/main.js
@@ -3,7 +3,13 @@ import GeraCPF from './modules/GeraCPF';
 import './assets/css/style.css';
 
 (function() {
+  const refreshbtn = document.querySelector('.refresh')
+  refreshbtn.addEventListener('click', refresh)
   const gera = new GeraCPF();
   const cpfGerado = document.querySelector('.cpf-gerado');
   cpfGerado.innerHTML = gera.geraNovoCpf();
 })();
+
+function refresh(){
+  window.location.reload();
+}
