e54f6bec2edce225380758ed69f28f208f56c5a4
Pedro <pedro.isaak@gmail.com>
2022-10-07 18:12:41 -0300

    Atualizado o controller

--- src/modules/auth/authController.ts
+++ src/modules/auth/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
+import { ILoginDTO } from "./IloginDto";
 
-export function login(user, password) {
+export function login({ email, password }: ILoginDTO) {
   try {
     //Login
   } catch (e) {
@@ -12,6 +13,6 @@ export function logOut(response: Response) {
   response.redirect(""); //redirect para o index quando for criado
 }
 
-export function forgotPassword(email: string) {
+export function forgotPassword(email: ILoginDTO) {
   //Buscar usuário no db e dar update no mesmo
 }
