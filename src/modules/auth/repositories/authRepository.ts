467c925f913c0592b7e3c9beae2efc9d3bcfbbf1
Pedro <pedro.isaak@gmail.com>
2022-10-07 19:31:26 -0300

    Criado o repository do auth

---
+++ src/modules/auth/repositories/authRepository.ts
@@ -0,0 +1,19 @@
+class AuthRepository {
+  private users: any; //Iria criar o DTO do repositório, mas não existe tal tarefa
+
+  private constructor() {
+    this.users = [];
+  }
+  create({ email, password }: any): void {
+    const user = new User(); //Criar o model
+
+    Object.assign(user, {
+      email,
+      password,
+      created_at: new Date(),
+    });
+
+    this.users.push(user);
+  }
+}
+export { AuthRepository };
