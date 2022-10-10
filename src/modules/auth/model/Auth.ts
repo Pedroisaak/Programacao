5f1fee74929ae1782d6182470bc3c195c15d58c1
Pedro <pedro.isaak@gmail.com>
2022-10-10 17:00:22 -0300

    Refatorado o model para uso do mongodb

--- src/modules/auth/model/Auth.ts
+++ src/modules/auth/model/Auth.ts
@@ -1,3 +1,22 @@
-class User {}
+import mongoose from "mongoose";
 
-export { User };
+const AuthSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const LoginModel = mongoose.model("Login", AuthSchema);
+
+class Login {
+  constructor() {}
+
+  async login() {}
+
+  async register() {}
+
+  async userExists() {}
+
+  validatedUser() {}
+}
+
+export { Login };
