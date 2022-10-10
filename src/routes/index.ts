d166a07b541662884b034acff8957cbc909f1ba1
Pedro <pedro.isaak@gmail.com>
2022-10-10 14:49:21 -0300

    Refeito o cors conforme solicitado

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { auth } from "../middleware/auth";
-import { cors } from "../middleware/cors";
 
 const router = Router();
 
-router.use("/", auth, cors, (request, response) => {
+router.use("/", auth, (request, response) => {
   return response.json({ message: "Route OK" });
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { router } from "./routes";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
