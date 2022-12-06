72bf53320e0be8423c42363335ca6870bdaff466
EmersonBraun <emersonfbraun@gmail.com>
2022-12-06 15:03:26 -0300

    feat: add ip list

--- src/sharred/bootstrap/index.ts
+++ src/sharred/bootstrap/index.ts
@@ -5,8 +5,10 @@ import setupDatabases from "./databases";
 import setupMiddlewares from "./middlewares";
 import setupBodyParser from "./body-parser";
 import setupFileupload from "./express-fileupload";
+import setupIp from "./list-ip";
 
 export const bootstrapApp = (app: Express): void => {
+  setupIp();
   setupFileupload(app);
   setupBodyParser(app);
   setupDatabases();
