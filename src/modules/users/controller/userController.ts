bf9b763216fc65833ff83e74df1e3f9a636eb3fa
luiscostalafc <luiscostalafc@gmail.com>
2023-01-13 07:39:54 -0300

    fix: some changes in project's configurations

--- src/modules/users/controller/userController.ts
+++ src/modules/users/controller/userController.ts
@@ -1,5 +1,11 @@
 import { HttpRequest, HttpResponse } from "../../../sharred/protocols";
-import { badRequest, created, notContent, ok, serverError } from "../../../sharred/services/http-helper";
+import {
+  badRequest,
+  created,
+  notContent,
+  ok,
+  serverError,
+} from "../../../sharred/services/http-helper";
 import logger from "../../../sharred/services/logger";
 import { UsersRepository } from "../repositories/UsersRepository";
 import { sendWelcomeEmail } from "../service/userService";
@@ -10,7 +16,7 @@ export async function listUser(): Promise<HttpResponse> {
     const response = await repository.list();
     return ok(response);
   } catch (error) {
-    logger.error(error)
+    logger.error(error);
     return serverError();
   }
 }
@@ -40,23 +46,28 @@ export async function showUser({ params }: HttpRequest): Promise<HttpResponse> {
     const response = await repository.show(params.id);
     return ok(response);
   } catch (error) {
-    logger.error(error)
+    logger.error(error);
     return serverError();
   }
 }
 
-export async function updateUser({ params, body }: HttpRequest): Promise<HttpResponse> {
+export async function updateUser({
+  params,
+  body,
+}: HttpRequest): Promise<HttpResponse> {
   const repository = new UsersRepository();
   try {
     const response = await repository.update(params.id, body);
     return ok(response);
   } catch (error) {
-    logger.error(error)
+    logger.error(error);
     return serverError();
   }
 }
 
-export async function deleteUser({ params }: HttpRequest): Promise<HttpResponse> {
+export async function deleteUser({
+  params,
+}: HttpRequest): Promise<HttpResponse> {
   const repository = new UsersRepository();
   try {
     await repository.delete(params.id);
